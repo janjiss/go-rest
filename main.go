172718c61bc488bbaa775848c8600eba3b0248f0
Janis Miezitis <janjiss@gmail.com>
2023-08-04 12:42:14 +0300

    Add .env

--- main.go
+++ main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
 	"sync"
 
+	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"janjiss.com/rest/users"
@@ -15,8 +18,13 @@ var wg sync.WaitGroup
 func main() {
 	wg.Add(1)
 
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
 	db, err := gorm.Open(postgres.New(postgres.Config{
-		DSN:                  "user=postgres dbname=gorm port=5432 sslmode=disable",
+		DSN:                  fmt.Sprintf("user=%s dbname=%s port=%s sslmode=%s", os.Getenv("DB_USER"), os.Getenv("DB_NAME"), os.Getenv("DB_PORT"), os.Getenv("DB_SSLMODE")),
 		PreferSimpleProtocol: true, // disables implicit prepared statement usage
 	}), &gorm.Config{})
 

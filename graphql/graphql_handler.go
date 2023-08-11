52139b22069e8760452887ec114e986c8d2b62c2
Janis Miezitis <janjiss@gmail.com>
2023-08-11 20:05:53 +0300

    Add graphql

---
+++ graphql/graphql_handler.go
@@ -0,0 +1,28 @@
+package graphqlHandler
+
+import (
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/gin-gonic/gin"
+	"janjiss.com/rest/graphql/graph"
+)
+
+// Defining the Graphql handler
+func GraphqlHandler() gin.HandlerFunc {
+	// NewExecutableSchema and Config are in the generated.go file
+	// Resolver is in the resolver.go file
+	h := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
+
+	return func(c *gin.Context) {
+		h.ServeHTTP(c.Writer, c.Request)
+	}
+}
+
+// Defining the Playground handler
+func PlaygroundHandler() gin.HandlerFunc {
+	h := playground.Handler("GraphQL", "/graphql")
+
+	return func(c *gin.Context) {
+		h.ServeHTTP(c.Writer, c.Request)
+	}
+}
--- web/server.go
+++ web/server.go
@@ -3,6 +3,7 @@ package web
 import (
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
+	graphqlHandler "janjiss.com/rest/graphql"
 	auth "janjiss.com/rest/login"
 	"janjiss.com/rest/users"
 )
@@ -11,6 +12,9 @@ func StartServer(db *gorm.DB) {
 	us := users.NewUserService(db)
 
 	r := gin.Default()
+	r.POST("/graphql", graphqlHandler.GraphqlHandler())
+	r.GET("/playground", graphqlHandler.PlaygroundHandler())
+
 	r.POST("/login", BuildLoginHandler(us))
 
 	authorized := r.Group("/")

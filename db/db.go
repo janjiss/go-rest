b026fcd3cec6f0b857ab944bd834b2573e76daab
Janis Miezitis <janjiss@gmail.com>
2023-08-20 19:13:47 +0300

    Add TOML db config

---
+++ db/db.go
@@ -0,0 +1,59 @@
+package db
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/pelletier/go-toml/v2"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"janjiss.com/rest/users"
+)
+
+type DBConfig struct {
+	DBName      string `toml:"db_name"`
+	DBUser      string `toml:"db_user"`
+	DBPort      string `toml:"db_port"`
+	DBEnableSSL string `toml:"db_enable_ssl"`
+	DBHost      string `toml:"db_host"`
+}
+
+func NewDB() *gorm.DB {
+	relativePath := "config/" + os.Getenv("APP_ENV") + ".toml"
+	absolutePath, err := filepath.Abs(relativePath)
+	configFile, err := os.ReadFile(absolutePath)
+
+	if err != nil {
+		panic("Failed to open config file")
+	}
+
+	var dbConfig DBConfig
+
+	fmt.Printf(string(configFile))
+
+	err = toml.Unmarshal(configFile, &dbConfig)
+
+	if err != nil {
+		panic("Failed to parse config file")
+	}
+
+	fmt.Println(dbConfig)
+
+	db, err := gorm.Open(postgres.New(postgres.Config{
+		DSN:                  fmt.Sprintf("user=%s dbname=%s port=%s sslmode=%s", dbConfig.DBUser, dbConfig.DBName, dbConfig.DBPort, dbConfig.DBEnableSSL),
+		PreferSimpleProtocol: true, // disables implicit prepared statement usage
+	}), &gorm.Config{})
+
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	db.AutoMigrate(users.User{})
+
+	if err != nil {
+		panic("Failed to migrate")
+	}
+
+	return db
+}
--- main.go
+++ main.go
@@ -1,15 +1,11 @@
 package main
 
 import (
-	"fmt"
 	"log"
-	"os"
 	"sync"
 
 	"github.com/joho/godotenv"
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
-	"janjiss.com/rest/users"
+	"janjiss.com/rest/db"
 	"janjiss.com/rest/web"
 )
 
@@ -19,25 +15,10 @@ func main() {
 	wg.Add(1)
 
 	err := godotenv.Load()
+	db := db.NewDB()
+
 	if err != nil {
 		log.Fatal("Error loading .env file")
-	}
-
-	db, err := gorm.Open(postgres.New(postgres.Config{
-		DSN:                  fmt.Sprintf("user=%s dbname=%s port=%s sslmode=%s", os.Getenv("DB_USER"), os.Getenv("DB_NAME"), os.Getenv("DB_PORT"), os.Getenv("DB_SSLMODE")),
-		PreferSimpleProtocol: true, // disables implicit prepared statement usage
-	}), &gorm.Config{})
-
-	if err != nil {
-		panic("failed to connect database")
-	}
-
-	db.AutoMigrate(users.User{})
-
-	if err != nil {
-		fmt.Printf("Err(s):\n%+v\n", err)
-
-		return
 	}
 
 	go web.StartServer(db)
